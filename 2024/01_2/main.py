795bb92610c66fa128c7633bf28c7695423d1d8e
Sepehr Shirkhanlou <sepehr.shirkhanlou@yahoo.com>
2024-12-02 13:13:11 +0330

    Solution for `2024/01_2`

--- 2024/01_2/main.py
+++ 2024/01_2/main.py
@@ -6,21 +6,29 @@ INPUT_FILE = "input.txt"
 RESULT_FILE = "result.txt"
 
 
-def read_input():
+def read_input() -> list[tuple[int, int]]:
     """Read input from file"""
     input_file = Path(__file__).parent / INPUT_FILE
-    raise NotImplementedError
+    with open(input_file, "r") as f:
+        return [tuple(map(int, line.strip().split())) for line in f.readlines()]
 
 
 def write_result(data: str):
     """Write results to a file"""
     result_file = Path(__file__).parent / RESULT_FILE
-    raise NotImplementedError
+    with open(result_file, "w") as f:
+        f.write(data)
 
 
 def main():
     """Solve the puzzle"""
-    raise NotImplementedError
+    left_list: list[int] = []
+    right_list: list[int] = []
+    for left, right in read_input():
+        left_list.append(left)
+        right_list.append(right)
+    similarity_scores = [left_point * right_list.count(left_point) for left_point in left_list]
+    write_result(str(sum(similarity_scores)))
 
 
 if __name__ == "__main__":
